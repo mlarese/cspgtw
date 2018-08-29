b92d01ad9b6ba3ea93f501b3e68430d29d9a44fe
mauro.larese <mauro.larese@mm-one.com>
2018-08-29 10:27:25 +0200

    update

--- ejb/src/main/java/com/ns/cspgtw/proxylayer/wind/IntProv26DTO.java
+++ ejb/src/main/java/com/ns/cspgtw/proxylayer/wind/IntProv26DTO.java
@@ -3,6 +3,137 @@ package com.ns.cspgtw.proxylayer.wind;
 import com.ns.cspgtw.proxylayer.AbstractProxyInvokerDTO;
 
 public class IntProv26DTO extends AbstractProxyInvokerDTO {
+    private String mt_carrier;
+    private String mt_authID;
+    private String mt_target;
+    private String mt_servtype;
+    private String mt_params;
+    private String mt_paramName1;
+    private String mt_paramValue1;
+    private String mt_paramName2;
+    private String mt_paramValue2;
+    private String mt_paramName3;
+    private String mt_paramValue3;
+    private String mt_paramName4;
+    private String mt_paramValue4;
+
+    public String getMt_carrier() {
+        return (String) this.get("mt_carrier");
+    }
+
+    public IntProv26DTO setMt_carrier(String mt_carrier) {
+        this.put("mt_carrier", mt_carrier);
+        return this;
+    }
+
+    public String getMt_authID() {
+        return (String) this.get("mt_authID");
+    }
+
+    public IntProv26DTO setMt_authID(String mt_authID) {
+        this.put("mt_authID", mt_authID);
+        return this;
+    }
+
+    public String getMt_target() {
+        return (String) this.get("mt_target");
+    }
+
+    public IntProv26DTO setMt_target(String mt_target) {
+        this.put("mt_target", mt_target);
+        return this;
+    }
+
+    public String getMt_servtype() {
+        return (String) this.get("mt_servtype");
+    }
+
+    public IntProv26DTO setMt_servtype(String mt_servtype) {
+        this.put("mt_servtype", mt_servtype);
+        return this;
+    }
+
+    public String getMt_params() {
+        return (String) this.get("mt_params");
+    }
+
+    public IntProv26DTO setMt_params(String mt_params) {
+        this.put("mt_params", mt_params);
+        return this;
+    }
+
+    public String getMt_paramName1() {
+        return (String) this.get("mt_paramName1");
+    }
+
+    public IntProv26DTO setMt_paramName1(String mt_paramName1) {
+        this.put("mt_paramName1", mt_paramName1);
+        return this;
+    }
+
+    public String getMt_paramValue1() {
+        return (String) this.get("mt_paramValue1");
+    }
+
+    public IntProv26DTO setMt_paramValue1(String mt_paramValue1) {
+        this.put("mt_paramValue1", mt_paramValue1);
+        return this;
+    }
+
+    public String getMt_paramName2() {
+        return (String) this.get("mt_paramName2");
+    }
+
+    public IntProv26DTO setMt_paramName2(String mt_paramName2) {
+        this.put("mt_paramName2", mt_paramName2);
+        return this;
+    }
+
+    public String getMt_paramValue2() {
+        return (String) this.get("mt_paramValue2");
+    }
+
+    public IntProv26DTO setMt_paramValue2(String mt_paramValue2) {
+        this.put("mt_paramValue2", mt_paramValue2);
+        return this;
+    }
+
+    public String getMt_paramName3() {
+        return (String) this.get("mt_paramName3");
+    }
+
+    public IntProv26DTO setMt_paramName3(String mt_paramName3) {
+        this.put("mt_paramName3", mt_paramName3);
+        return this;
+    }
+
+    public String getMt_paramValue3() {
+        return (String) this.get("mt_paramValue3");
+    }
+
+    public IntProv26DTO setMt_paramValue3(String mt_paramValue3) {
+        this.put("mt_paramValue3", mt_paramValue3);
+        return this;
+    }
+
+    public String getMt_paramName4() {
+        return (String) this.get("mt_paramName4");
+    }
+
+    public IntProv26DTO setMt_paramName4(String mt_paramName4) {
+        this.put("mt_paramName4", mt_paramName4);
+        return this;
+    }
+
+    public String getMt_paramValue4() {
+        return (String) this.get("mt_paramValue4");
+    }
+
+    public IntProv26DTO setMt_paramValue4(String mt_paramValue4) {
+        this.put("mt_paramValue4", mt_paramValue4);
+        return this;
+    }
+
     @Override
     public void reset() {
 
--- ejb/src/main/java/com/ns/cspgtw/service/api/billingasynchresp/BillingAsynchRespResponseBuilderWind.java
+++ ejb/src/main/java/com/ns/cspgtw/service/api/billingasynchresp/BillingAsynchRespResponseBuilderWind.java
@@ -1,8 +1,13 @@
 package com.ns.cspgtw.service.api.billingasynchresp;
 
+import com.ns.cspgtw.proxylayer.ProxyInvokeResult;
+import com.ns.cspgtw.proxylayer.wind.IntPb02DTO;
+import com.ns.cspgtw.proxylayer.wind.intProv03DTO;
+import com.ns.cspgtw.service.api.smscaringdelivery.SmsCaringDeliveryRequest;
 import com.ns.cspgtw.service.builder.AbstractBuilder;
 import com.ns.cspgtw.service.builder.Request;
 import com.ns.cspgtw.service.builder.Resources;
+import com.ns.cspgtw.service.enums.ResultCodesEnum;
 
 public class BillingAsynchRespResponseBuilderWind extends AbstractBuilder<BillingAsynchRespResponse, BillingAsynchRespRequest> {
 
@@ -12,6 +17,42 @@ public class BillingAsynchRespResponseBuilderWind extends AbstractBuilder<Billin
 
     @Override
     public BillingAsynchRespResponse build() {
-        return null;
+        ResultCodesEnum resEnum = ResultCodesEnum.RC1002;
+        try {
+            int invokeResult = callOperatorApi();
+            return null;
+        } catch (Exception e) {
+            resEnum = ResultCodesEnum.RC2000;
+        }
+
+
+        BillingAsynchRespResponse billingAsynchRespResponse = new BillingAsynchRespResponse();
+        billingAsynchRespResponse.setResultCode(0);
+
+        return billingAsynchRespResponse;
+    }
+
+    protected static ResultCodesEnum getByApiCall (int resultCode) {
+        switch (resultCode) {
+            case 0 : return ResultCodesEnum.RC1002;
+            default: return ResultCodesEnum.RC2040;
+        }
+    }
+
+    private int callOperatorApi()  {
+        // dto is the request
+        intProv03DTO dto = new intProv03DTO(  IntPb02DTO.url   );
+
+
+        dto
+            .setMtId( getRequest().getMtId())
+            .setArErrorcode(getRequest().getArErrorCode())
+            .setMtAuthcode(getRequest().getMtAuthCode())
+            .setMtTarget(getRequest().getMtTarget())
+        ;
+
+        // chiama api wind e restituisce un numero
+        return getResources().invokeProxy(dto).getResult();
+
     }
 }
